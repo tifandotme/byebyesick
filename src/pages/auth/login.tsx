ed3012b884ac741784dd5c6a5d56e5c91872cb11
Muhammad Yafi Al Hakim <yafi.alhakim@shopee.com>
2023-12-27 15:04:24 +0700

    feat(user): initiate auth pages register and login, and init component setup

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -15,8 +15,6 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
-// import { toast } from "@/components/ui/use-toast"
-
 const FormSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
