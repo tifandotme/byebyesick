84da26599ad95d283d35d56024af3ab5ea94ab12
Muhammad Yafi Al Hakim <yafi.alhakim@shopee.com>
2023-12-27 15:27:56 +0700

    fix(user): initiate auth pages register and login, and init component setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
-import { Html, Main, NextScript } from "next/document"
+import { Head, Html, Main, NextScript } from "next/document"
 
 import { fonts } from "@/lib/fonts"
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Head />
+
       <body className={fonts.map((font) => font.className).join(" ")}>
         <Main />
         <NextScript />
