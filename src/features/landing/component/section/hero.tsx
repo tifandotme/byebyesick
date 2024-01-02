6561616a4a2b20bc0a79d5836e434efa205c5ef2
Muhammad Yafi Al Hakim <yafi.alhakim@shopee.com>
2024-01-02 18:09:44 +0700

    feat(user): initiate homepage hero and carousel

--- src/features/landing/component/section/hero.tsx
+++ src/features/landing/component/section/hero.tsx
@@ -10,43 +10,42 @@ import {
 } from "@/components/ui/carousel"
 
 function Hero() {
-  const data = ["a", "b", "c", "d"]
+  const contentText = ["a", "b", "c"]
   return (
-    <div className="p-5">
-      <Carousel className="w-full">
-        <CarouselPrevious />
-        <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
-              <div className="flex">
-                <div className="p-1">
+    <>
+      <div className="container max-w-6xl pt-0 md:pt-0">
+        <section className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-4 py-12 text-center md:pt-32">
+          <h1 className="font-heading text-balance text-center text-3xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
+            Your One Stop Health Solutions
+          </h1>
+          <p className="mt-6 max-w-6xl text-center leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            Chat with doctors, visit hospitals, buy medicine, check labs and
+            updates Information about health can all be found at ByeByeSick!
+          </p>
+        </section>
+        <div className="p-5">
+          <Carousel className="w-full">
+            <CarouselPrevious />
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
                   <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-4xl font-semibold">
-                        {data[index]}
+                    <CardContent className="flex items-center justify-center p-6">
+                      <span className="text-4xl font-semibold ">
+                        {contentText.map((c, index) => (
+                          <div key={index}>{c}</div>
+                        ))}
                       </span>
                     </CardContent>
                   </Card>
-                </div>
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-4xl font-semibold">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ratione nam iusto, minus vitae maiores doloremque,
-                        excepturi ipsa esse necessitatibus voluptatum est
-                        accusamus sequi illum. Dignissimos.
-                      </span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselNext />
-      </Carousel>
-    </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselNext />
+          </Carousel>
+        </div>
+      </div>
+    </>
   )
 }
 
