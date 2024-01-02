183efd5880a67845a9dd0aac60f33cc8832fb88b
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-02 19:03:54 +0700

    feat: add verification page

--- src/features/auth/components/form/verifyForm.tsx
+++ src/features/auth/components/form/verifyForm.tsx
@@ -1,9 +1,12 @@
 import React from "react"
+import Image from "next/image"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm, type SubmitHandler } from "react-hook-form"
 import { toast } from "sonner"
 
 import type { RegisterToken } from "@/types/user"
+import doctor from "@/assets/backgrounds/doctor.svg"
+import patient from "@/assets/backgrounds/patient.svg"
 import {
   verifyFormSchema,
   type VerifyFormSchemaType,
@@ -18,11 +21,14 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 function VerifyForm({ data }: RegisterToken) {
   const form = useForm<VerifyFormSchemaType>({
     resolver: zodResolver(verifyFormSchema),
   })
+
+  const [isDoctor, setIsDoctor] = React.useState<boolean>(false)
 
   const onSubmit: SubmitHandler<VerifyFormSchemaType> = async (data) => {
     try {
@@ -44,7 +50,66 @@ function VerifyForm({ data }: RegisterToken) {
         onSubmit={form.handleSubmit(onSubmit)}
         className="w-full space-y-6 lg:w-1/2"
       >
-        <div className="flex flex-col"></div>
+        <div className="text-xl">
+          Welcome{" "}
+          <span className="text-3xl font-medium text-primary">{data}</span>
+        </div>
+        <div>
+          <FormLabel>Register as</FormLabel>
+          <fieldset className="grid grid-cols-2 place-content-center gap-4 p-2">
+            <legend className="sr-only">Role</legend>
+            <div>
+              <input
+                type="radio"
+                value="3"
+                onClick={() => {
+                  setIsDoctor(true)
+                }}
+                {...form.register("role")}
+                id="doctor"
+                className="peer hidden"
+              />
+              <label
+                htmlFor="doctor"
+                className="flex cursor-pointer flex-col items-center rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 peer-checked:border-primary peer-checked:ring-1 peer-checked:ring-primary"
+              >
+                <Image
+                  src={doctor}
+                  alt="doctor"
+                  className="container h-20 object-fill"
+                  width={200}
+                  height={200}
+                />
+                <p className="mt-1 text-gray-900">Doctor</p>
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                value="4"
+                onClick={() => {
+                  setIsDoctor(false)
+                }}
+                {...form.register("role")}
+                id="patient"
+                className="peer hidden"
+              />
+              <label
+                htmlFor="patient"
+                className="flex cursor-pointer flex-col items-center rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 peer-checked:border-primary peer-checked:ring-1 peer-checked:ring-primary"
+              >
+                <Image
+                  src={patient}
+                  alt="doctor"
+                  className="container h-20 object-contain"
+                  width={200}
+                  height={200}
+                />
+                <p className="mt-1 text-gray-900">Patient</p>
+              </label>
+            </div>
+          </fieldset>
+        </div>
         <FormField
           control={form.control}
           name="password"
@@ -71,6 +136,12 @@ function VerifyForm({ data }: RegisterToken) {
             </FormItem>
           )}
         />
+        {isDoctor && (
+          <div className="grid w-full max-w-sm items-center gap-2">
+            <Label htmlFor="picture">Doctor Certificate</Label>
+            <Input id="picture" type="file" />
+          </div>
+        )}
         <Button className="w-full" type="submit">
           Submit
         </Button>
