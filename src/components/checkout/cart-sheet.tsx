dfd2e7e2c8e2fc99dece1a8f9af74b26c0d5b3b5
Muhammad Yafi Al Hakim <yafi.alhakim@shopee.com>
2024-01-02 21:34:15 +0700

    feat(user): initiate ui cart

---
+++ src/components/checkout/cart-sheet.tsx
@@ -0,0 +1,111 @@
+import React from "react"
+import Link from "next/link"
+import { Separator } from "@radix-ui/react-dropdown-menu"
+import { ShoppingCart } from "lucide-react"
+
+import { cn, formatPrice } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+export default function CartSheet() {
+  const itemCount = 0
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          aria-label="Open cart"
+          variant="outline"
+          size="icon"
+          className="relative"
+        >
+          {/* {itemCount > 0 && (
+            <Badge
+              variant="secondary"
+              className="absolute -right-2 -top-2 h-6 w-6 justify-center rounded-full p-2.5"
+            >
+              {itemCount}
+            </Badge>
+          )} */}
+          <ShoppingCart className="h-4 w-4" aria-hidden="true" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+        <SheetHeader className="space-y-2.5 pr-6">
+          {/* <SheetTitle>Cart {itemCount > 0 && `(${itemCount})`}</SheetTitle> */}
+          <SheetTitle>Cart</SheetTitle>
+
+          <Separator />
+        </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            {/* <CartLineItems items={cartLineItems} className="flex-1" /> */}
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Taxes</span>
+                  <span>Calculated at checkout</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  {/* <span>{formatPrice(cartTotal.toFixed(2))}</span> */}
+                </div>
+              </div>
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    aria-label="View your cart"
+                    href="/cart"
+                    className={buttonVariants({
+                      size: "sm",
+                      className: "w-full",
+                    })}
+                  >
+                    Continue to checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <ShoppingCart
+              className="mb-4 h-16 w-16 text-muted-foreground"
+              aria-hidden="true"
+            />
+            <div className="text-xl font-medium text-muted-foreground">
+              Your cart is empty
+            </div>
+            <SheetTrigger asChild>
+              <Link
+                aria-label="Add items to your cart to checkout"
+                href="/products"
+                className={cn(
+                  buttonVariants({
+                    variant: "link",
+                    size: "sm",
+                    className: "text-sm text-muted-foreground",
+                  }),
+                )}
+              >
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
+          </div>
+        )}
+      </SheetContent>
+    </Sheet>
+  )
+}
