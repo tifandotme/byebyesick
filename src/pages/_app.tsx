4ca7c68e3017ac996d00ed87ad8d128cf9d7a11b
Tifan <tifan.avianto@shopee.com>
2024-01-02 23:50:23 +0700

    refactor: update _app and main layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
+import React from "react"
 import { ThemeProvider } from "next-themes"
 import { SWRConfig } from "swr"
 
 import type { AppPropsWithLayout } from "@/types/next"
 import { fetcher } from "@/lib/fetchers"
+import { useStore } from "@/lib/stores/pharmacies"
 import { Toaster } from "@/components/ui/sonner"
 
 import "@/styles/globals.css"
+import "leaflet/dist/leaflet.css"
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
+
+  React.useEffect(() => {
+    useStore.persist.rehydrate()
+  }, [])
 
   return (
     <SWRConfig
@@ -18,11 +25,9 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       }}
     >
       <ThemeProvider attribute="class">
-        {getLayout(
-          <>
-            <Component {...pageProps} /> <Toaster richColors closeButton />
-          </>,
-        )}
+        {getLayout(<Component {...pageProps} />)}
+
+        <Toaster richColors closeButton />
       </ThemeProvider>
     </SWRConfig>
   )
