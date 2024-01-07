b0e5e40d377a99ce4c86a913241a3a8d1faaac12
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-07 22:13:20 +0700

    feat:  layout full screen

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -1,14 +1,16 @@
 import React from "react"
 
+import { SiteFooter } from "@/components/layouts/site-footer"
 import MainNavbar from "@/components/navbar/mainNavbar"
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen">
+    <div className="flex h-screen flex-col overflow-auto">
       <div className="sticky w-full">
         <MainNavbar />
       </div>
-      <div className="container py-11">{children}</div>
+      <div className="container grow py-11">{children}</div>
+      <SiteFooter />
     </div>
   )
 }
