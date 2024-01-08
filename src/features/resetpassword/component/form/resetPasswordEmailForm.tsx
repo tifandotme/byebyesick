76720defdee88b46d46cef3c42a4afd01c3b0fd9
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-08 14:16:49 +0700

    feat: slicing reset password feature

--- src/features/resetpassword/component/form/resetPasswordEmailForm.tsx
+++ src/features/resetpassword/component/form/resetPasswordEmailForm.tsx
@@ -1,7 +1,68 @@
 import React from "react"
+import { useRouter } from "next/router"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import type { ResetPasswordEmailSchemeType } from "@/types"
+import { resetPasswordEmailScheme } from "@/lib/validations/reset-password"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 function ResetPasswordEmailForm() {
-  return <div></div>
+  const router = useRouter()
+  const [isLoading, setIsLoading] = React.useState(false)
+  const form = useForm<ResetPasswordEmailSchemeType>({
+    resolver: zodResolver(resetPasswordEmailScheme),
+  })
+
+  const onSubmit = async () => {
+    setIsLoading(true)
+
+    setIsLoading(false)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full space-y-6 "
+      >
+        <h1 className="text-xl font-medium">Reset Password</h1>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="johndoe@gmail.com"
+                  type="email"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                Enter valid email to send verification link
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" disabled={isLoading} type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  )
 }
 
 export default ResetPasswordEmailForm
--- src/features/resetpassword/component/form/resetPasswordForm.tsx
+++ src/features/resetpassword/component/form/resetPasswordForm.tsx
@@ -1,7 +1,76 @@
 import React from "react"
+import { useRouter } from "next/router"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import type { NewPasswordEmailSchemeType } from "@/types"
+import { newPasswordScheme } from "@/lib/validations/reset-password"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 function ResetPasswordForm() {
-  return <div>ResetPasswordForm</div>
+  const router = useRouter()
+  const [isLoading, setIsLoading] = React.useState(false)
+  const form = useForm<NewPasswordEmailSchemeType>({
+    resolver: zodResolver(newPasswordScheme),
+  })
+  const onSubmit = async () => {
+    setIsLoading(true)
+
+    setIsLoading(false)
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full space-y-6 "
+      >
+        <h1 className="text-xl font-medium">Set New Password</h1>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>New Password</FormLabel>
+              <FormControl>
+                <Input placeholder="******" type="password" {...field} />
+              </FormControl>
+              <FormDescription>
+                Enter minimum 8 character for password
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm New Password</FormLabel>
+              <FormControl>
+                <Input placeholder="******" type="password" {...field} />
+              </FormControl>
+              <FormDescription>Confirm your new Password</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" disabled={isLoading} type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  )
 }
 
 export default ResetPasswordForm
