9a1290135f18cb383d8c58227b14ce508e0c9e59
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-08 13:00:57 +0700

    feat: reset password layout

--- src/features/auth/components/form/loginForm.tsx
+++ src/features/auth/components/form/loginForm.tsx
@@ -113,7 +113,7 @@ export default function LoginForm() {
               Register
             </Link>
           </div>
-          <Link href={"/"} className="text-apple-600">
+          <Link href={"/reset/password"} className="text-apple-600">
             Reset Password
           </Link>
         </div>
