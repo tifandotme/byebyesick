ed525cc90fa663f16451ce33c4019d72b7cc3c8b
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-08 14:16:49 +0700

    feat: slicing reset password feature

--- src/features/resetpassword/component/layout/reset-password.tsx
+++ src/features/resetpassword/component/layout/reset-password.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/components/ui/card"
 function ResetPasswordLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-row items-center justify-center">
-      <Card>{children}</Card>
+      <Card className="w-full max-w-2xl p-5">{children}</Card>
     </div>
   )
 }
