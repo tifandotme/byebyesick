68e9ca549b49cd9471123ae310605d61bd555abe
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-09 16:52:04 +0700

    feat: validate token in reset password

--- src/pages/reset/password.tsx
+++ src/pages/reset/password.tsx
@@ -1,10 +1,18 @@
 import React from "react"
+import Head from "next/head"
 
 import ResetPasswordEmailForm from "@/features/resetpassword/component/form/resetPasswordEmailForm"
 import ResetPasswordLayout from "@/features/resetpassword/component/layout/reset-password"
 
 function PasswordPage() {
-  return <ResetPasswordEmailForm />
+  return (
+    <>
+      <Head>
+        <title>ByeByeSick | Forget Password</title>
+      </Head>
+      <ResetPasswordEmailForm />
+    </>
+  )
 }
 PasswordPage.getLayout = function getLayout(page: React.ReactElement) {
   return <ResetPasswordLayout>{page}</ResetPasswordLayout>
