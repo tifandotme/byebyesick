86b7dfe9984fdbeac1105c2f96ae8cf2f2931036
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-09 15:15:04 +0700

    feat: add container in reset password

--- src/features/resetpassword/component/layout/reset-password.tsx
+++ src/features/resetpassword/component/layout/reset-password.tsx
@@ -1,11 +1,19 @@
 import React from "react"
+import Image from "next/image"
 
 import { Card } from "@/components/ui/card"
 
 function ResetPasswordLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-row items-center justify-center">
-      <Card className="w-full max-w-2xl p-5">{children}</Card>
+    <div className="container flex h-screen flex-col items-center justify-center">
+      <Image
+        src="/logo.png"
+        alt="Logo png"
+        width={150}
+        className="-translate-y-5"
+        height={50}
+      />
+      <Card className="w-full max-w-lg -translate-y-5 p-5">{children}</Card>
     </div>
   )
 }
