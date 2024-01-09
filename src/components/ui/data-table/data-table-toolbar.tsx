91b5133c3dbd8cff84027c6e6ad0761c13763c56
Tifan <tifan.avianto@shopee.com>
2024-01-09 16:50:22 +0700

    feat: handle server-side, sorting, filter, pagination

--- src/components/ui/data-table/data-table-toolbar.tsx
+++ src/components/ui/data-table/data-table-toolbar.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useRouter } from "next/router"
 import { Cross2Icon } from "@radix-ui/react-icons"
 import type { Table } from "@tanstack/react-table"
 
@@ -6,6 +7,7 @@ import type {
   DataTableFilterableColumn,
   DataTableSearchableColumn,
 } from "@/types"
+import { useDebounce } from "@/hooks/use-debounce"
 import { Button } from "@/components/ui/button"
 import { DataTableFilter } from "@/components/ui/data-table/data-table-filter"
 import { DataTableViewOptions } from "@/components/ui/data-table/data-table-view-options"
@@ -20,34 +22,72 @@ interface DataTableToolbarProps<TData> {
 export function DataTableToolbar<TData>({
   table,
   filterableColumns = [],
-  searchableColumns = [],
 }: DataTableToolbarProps<TData>) {
+  const router = useRouter()
+
   const isFiltered = table.getState().columnFilters.length > 0
+
+  const searchParams = React.useMemo(
+    () => new URLSearchParams(router.asPath.split("?")[1]),
+    [router.asPath],
+  )
+
+  const createQueryString = React.useCallback(
+    (params: Record<string, string | number | null>) => {
+      const newSearchParams = searchParams
+
+      for (const [key, value] of Object.entries(params)) {
+        if (value === null) {
+          newSearchParams.delete(key)
+        } else {
+          newSearchParams.set(key, String(value))
+        }
+      }
+
+      return newSearchParams.toString()
+    },
+    [searchParams],
+  )
+
+  const [query, setQuery] = React.useState(router.query.search ?? "")
+  const debouncedQuery = useDebounce(query, 500)
+
+  React.useEffect(
+    () => {
+      if (debouncedQuery) {
+        router.replace(
+          {
+            pathname: router.asPath.split("?")[0],
+            query: createQueryString({ search: debouncedQuery }),
+          },
+          undefined,
+          { shallow: true },
+        )
+      } else {
+        router.replace(
+          {
+            pathname: router.asPath.split("?")[0],
+            query: createQueryString({ search: null }),
+          },
+          undefined,
+          { shallow: true },
+        )
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [debouncedQuery],
+  )
 
   return (
     <div className="flex w-full items-center justify-between space-x-2 overflow-auto p-1">
       <div className="flex flex-1 items-center space-x-2">
-        {searchableColumns.length > 0 &&
-          searchableColumns.map(
-            (column) =>
-              table.getColumn(column.id ? String(column.id) : "") && (
-                <Input
-                  key={String(column.id)}
-                  placeholder={`Filter ${column.title}...`}
-                  value={
-                    (table
-                      .getColumn(String(column.id))
-                      ?.getFilterValue() as string) ?? ""
-                  }
-                  onChange={(event) =>
-                    table
-                      .getColumn(String(column.id))
-                      ?.setFilterValue(event.target.value)
-                  }
-                  className="h-8 w-[150px] lg:w-[250px]"
-                />
-              ),
-          )}
+        <Input
+          placeholder={`Search...`}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="h-8 w-[150px] lg:w-[250px]"
+        />
+
         {filterableColumns.length > 0 &&
           filterableColumns.map(
             (column) =>
