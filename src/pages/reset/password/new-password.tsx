4da8bb88af708c244cf30d27371558081b3a685b
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-09 16:52:04 +0700

    feat: validate token in reset password

--- src/pages/reset/password/new-password.tsx
+++ src/pages/reset/password/new-password.tsx
@@ -1,9 +1,34 @@
 import React from "react"
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next"
 
+import type { RegisterToken } from "@/types/user"
 import ResetPasswordForm from "@/features/resetpassword/component/form/resetPasswordForm"
 import ResetPasswordLayout from "@/features/resetpassword/component/layout/reset-password"
 
-function NewPasswordPage() {
+export const getServerSideProps = (async (context) => {
+  const token = context?.query?.token
+  try {
+    const verify = await fetch(
+      `${process.env.NEXT_PUBLIC_DB_URL}v1/auth/verify-forgot?token=${token}`,
+    )
+    if (!verify.ok || verify.status === 400) {
+      throw new Error("Invalid Token")
+    }
+    const props: RegisterToken = await verify.json()
+    return { props: { props } }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
+  }
+}) satisfies GetServerSideProps<{
+  props: RegisterToken
+}>
+
+function NewPasswordPage({
+  props,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(props)
   return <ResetPasswordForm />
 }
 
