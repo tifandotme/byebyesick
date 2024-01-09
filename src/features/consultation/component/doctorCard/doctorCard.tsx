e9fbec7afc69250b4ab73e9ed57cd1c8bfff1718
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-09 15:51:36 +0700

    feat: add experience in doctor card

--- src/features/consultation/component/doctorCard/doctorCard.tsx
+++ src/features/consultation/component/doctorCard/doctorCard.tsx
@@ -14,16 +14,17 @@ interface DoctorCardI {
   name: string
   image: string
   spesialization: string
+  experience: number
 }
 
-function DoctorCard({ name, image, spesialization }: DoctorCardI) {
+function DoctorCard({ name, image, spesialization, experience }: DoctorCardI) {
   return (
     <Link href={"#"}>
       <Card
         className={`relative flex h-full min-h-40 w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-transparent text-primary shadow-md transition-colors hover:bg-muted/50`}
       >
         <CardHeader>
-          <div className="grid h-32 w-32 place-items-center rounded-full border-2">
+          <div className="grid h-24 w-24 place-items-center rounded-full border-2 xl:h-32 xl:w-32">
             <img
               src={image}
               alt={image}
@@ -39,6 +40,9 @@ function DoctorCard({ name, image, spesialization }: DoctorCardI) {
           <React.Suspense
             fallback={<Skeleton className="h-4 w-20" />}
           ></React.Suspense>
+          <div className="rounded-full bg-muted px-2 text-sm text-muted-foreground">
+            {experience} years
+          </div>
         </CardContent>
       </Card>
     </Link>
