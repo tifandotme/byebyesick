e3fb002669adec973d1f03749d34708197aa61eb
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-10 22:09:05 +0700

    refactor: reset password layout not full

--- src/features/resetpassword/component/form/resetPasswordEmailForm.tsx
+++ src/features/resetpassword/component/form/resetPasswordEmailForm.tsx
@@ -7,6 +7,7 @@ import { toast } from "sonner"
 import type { ResetPasswordEmailSchemeType } from "@/types"
 import { resetPasswordEmailScheme } from "@/lib/validations/reset-password"
 import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
 import {
   Form,
   FormControl,
@@ -32,7 +33,7 @@ function ResetPasswordEmailForm() {
     try {
       const result = await forgetPassword(data.email)
       if (!result?.ok) {
-        throw new Error(result.statusText)
+        throw new Error("Failed to Send Email")
       }
       toast.success("Verification Email Sent", { duration: 2000 })
     } catch (error) {
@@ -43,34 +44,39 @@ function ResetPasswordEmailForm() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-        <h1 className="text-xl font-medium">Reset Password</h1>
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="johndoe@gmail.com"
-                  type="email"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>
-                Enter valid email to send verification link
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button className="w-full" disabled={isLoading} type="submit">
-          Submit
-        </Button>
-      </form>
-    </Form>
+    <Card className="w-full max-w-lg -translate-y-5 p-5">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-6"
+        >
+          <h1 className="text-xl font-medium">Reset Password</h1>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="johndoe@gmail.com"
+                    type="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Enter valid email to send verification link
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="w-full" disabled={isLoading} type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </Card>
   )
 }
 
