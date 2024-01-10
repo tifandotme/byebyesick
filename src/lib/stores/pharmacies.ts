07ef476b4dd7048f00b8b34a82217b91596bc02f
Tifan <tifan.avianto@shopee.com>
2024-01-10 19:20:51 +0700

    refactor: move rajaongkir to the back end

--- src/lib/stores/pharmacies.ts
+++ src/lib/stores/pharmacies.ts
@@ -1,19 +1,15 @@
 import { create } from "zustand"
 import { devtools, persist } from "zustand/middleware"
 
-import type { City, Province } from "@/types/rajaongkir"
+import type { City, Province } from "@/types/api"
 
 type State = {
   isHydrating: boolean
-  provinceId: string | null
-  opensAt: string | null
   provinces: Province[] | null
   cities: City[] | null
 }
 
 type Actions = {
-  updateProvinceId: (id: string | null) => void
-  updateOpensAt: (opensAt: string | null) => void
   updateProvinces: (provinces: Province[]) => void
   updateCities: (cities: City[]) => void
 }
@@ -23,13 +19,9 @@ export const useStore = create<State & Actions>()(
     persist(
       (set) => ({
         isHydrating: true,
-        provinceId: null,
-        opensAt: null,
         provinces: null,
         cities: null,
 
-        updateProvinceId: (provinceId) => set(() => ({ provinceId })),
-        updateOpensAt: (opensAt) => set(() => ({ opensAt })),
         updateProvinces: (provinces) => set(() => ({ provinces })),
         updateCities: (cities) => set(() => ({ cities })),
       }),
