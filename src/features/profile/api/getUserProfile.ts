956407e879ebb76d27179db17a4499d5933eca2a
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-10 15:52:07 +0700

    feat: get user profile

---
+++ src/features/profile/api/getUserProfile.ts
@@ -0,0 +1,7 @@
+export async function getUserProfile(token: string | undefined) {
+  var requestOptions: RequestInit = {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  }
+  return fetch(`${process.env.BASE_URL}/v1/profile/user`, requestOptions)
+}
--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,12 +1,23 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Head from "next/head"
+import { useSession } from "next-auth/react"
 
 import MainLayout from "@/components/layout/mainLayout"
+import { getUserProfile } from "@/features/profile/api/getUserProfile"
 import ProfileForm from "@/features/profile/components/form/profileForm"
 import ProfileLayout from "@/features/profile/components/layout/profileLayout"
 import ResetPassword from "@/features/profile/components/resetPassword/resetPassword"
 
 function ProfilePage() {
+  const { data: session, status } = useSession()
+
+  useEffect(() => {
+    const getProfile = async () => {
+      const data = await getUserProfile(session?.user.token)
+      return data
+    }
+    if (status === "authenticated") console.log(getProfile())
+  }, [status])
   return (
     <ProfileLayout title="Profile" desc="User Profile Management">
       <Head>
