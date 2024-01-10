e06958932ed40a055e6a0d7839dac4ad899d516e
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-10 22:09:05 +0700

    refactor: reset password layout not full

--- src/features/resetpassword/component/layout/reset-password.tsx
+++ src/features/resetpassword/component/layout/reset-password.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import Image from "next/image"
 
-import { Card } from "@/components/ui/card"
-
 function ResetPasswordLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container flex h-screen flex-col items-center justify-center">
@@ -13,7 +11,7 @@ function ResetPasswordLayout({ children }: { children: React.ReactNode }) {
         className="-translate-y-5"
         height={50}
       />
-      <Card className="w-full max-w-lg -translate-y-5 p-5">{children}</Card>
+      <div className="flex w-full justify-center">{children}</div>
     </div>
   )
 }
