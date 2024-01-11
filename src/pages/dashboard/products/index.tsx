835798d352794face4ca020bc6b1a3b0ce7ae102
Muhammad Yafi Al Hakim <yafi.alhakim@shopee.com>
2024-01-11 22:38:27 +0700

    fix(admin): fix product and product categories have weird pagination'

--- src/pages/dashboard/products/index.tsx
+++ src/pages/dashboard/products/index.tsx
@@ -3,6 +3,11 @@ import { useRouter } from "next/router"
 import useSWR from "swr"
 
 import type { IProduct, ResponseGetAll } from "@/types/api"
+import {
+  getDrugClassificationName,
+  getManufacturerName,
+  getProductCategoryName,
+} from "@/lib/fetchers"
 import { DataTableSkeleton } from "@/components/ui/data-table/data-table-skeleton"
 import { DashboardLayout } from "@/components/layouts/dashboard"
 import { ProductTable } from "@/features/admin/table/product-table"
@@ -10,13 +15,14 @@ import ProductLayout from "@/features/products/components/layout"
 
 export default function ProductTablePage() {
   const router = useRouter()
+
   const { page, per_page, search, sort } = router.query
 
   const { data, isLoading, mutate } = useSWR<ResponseGetAll<IProduct[]>>(() => {
     const params = new URLSearchParams()
-    if (page) params.set("page", page)
-    if (per_page) params.set("limit", per_page)
+    if (per_page) params.set("per_page", per_page)
     if (search) params.set("search", search)
+    if (page) params.set("page", page)
     if (sort) params.set("sort_by", sort.split(".")[0] as string)
     if (sort) params.set("sort", sort.split(".")[1] as string)
 
