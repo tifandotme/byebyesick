5c9ce15d5f35712f7663198e82cd518d7768d24d
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-11 12:27:58 +0700

    feat: reset password button in profile

--- src/features/profile/components/resetPassword/resetPassword.tsx
+++ src/features/profile/components/resetPassword/resetPassword.tsx
@@ -1,10 +1,25 @@
 import React from "react"
+import { toast } from "sonner"
 
+import type { IProfileDoctor, IProfileUser } from "@/types/api"
 import { Button } from "@/components/ui/button"
 import { CardDescription, CardTitle } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
+import { forgetPassword } from "@/features/resetpassword/api/forgetPassword"
 
-function ResetPassword() {
+function ResetPassword(props: IProfileUser | IProfileDoctor) {
+  const resetPassword = async () => {
+    try {
+      const resp = await forgetPassword(props.email)
+      if (!resp.ok) {
+        throw new Error("Failed to send reset email")
+      }
+      toast.success("Reset email has sent")
+    } catch (error) {
+      const err = error as Error
+      toast.error(err.message)
+    }
+  }
   return (
     <>
       <div className="mt-10 space-y-1">
@@ -12,7 +27,13 @@ function ResetPassword() {
         <CardDescription>Send reset password request to email</CardDescription>
       </div>
       <Separator className="my-4" />
-      <Button type="button" variant={"destructive"}>
+      <Button
+        type="button"
+        onClick={() => {
+          resetPassword()
+        }}
+        variant={"destructive"}
+      >
         Reset Password
       </Button>
     </>
