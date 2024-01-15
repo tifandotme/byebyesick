14a2423f3d51dd70543aa913b124bd1695af413a
Tifan <tifan.avianto@shopee.com>
2024-01-15 09:47:49 +0700

    refactor: add font variables to body, edit css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,18 @@
 import { Head, Html, Main, NextScript } from "next/document"
 
 import { fonts } from "@/lib/fonts"
+import { cn } from "@/lib/utils"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={fonts.map((font) => font.className).join(" ")}>
+      <body
+        className={cn(
+          fonts.map((font) => font.variable),
+          "font-sans",
+        )}
+      >
         <Main />
         <NextScript />
       </body>
