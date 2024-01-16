240354021daba83aa91bcaf90ca3ebca37214de3
Mohammad Wasik Amin <wasik.amin@shopee.com>
2024-01-16 16:56:43 +0700

    feat: doctor page

--- src/features/consultation/component/doctorCategory/doctorCategory.tsx
+++ src/features/consultation/component/doctorCategory/doctorCategory.tsx
@@ -1,32 +1,27 @@
 import React from "react"
 
+import type { Specialization } from "@/types/api"
 import { CategoryCard } from "@/features/landing/component/categories/category-card"
 
-function DoctorCategory() {
+function DoctorCategory({ category }: { category: Specialization[] }) {
   return (
     <>
       <h1 className="text-2xl font-semibold text-primary">Doctor Categories</h1>
       <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
-        <CategoryCard
-          link="/constultation/categories"
-          category="Dokter Kandungan"
-          background="https://www.saarmagazine.nl/wp-content/uploads/2017/07/dokter-.jpg"
-        />
-        <CategoryCard
-          link="/constultation/categories"
-          category="Dokter Anak"
-          background="https://www.saarmagazine.nl/wp-content/uploads/2017/07/dokter-.jpg"
-        />
-        <CategoryCard
-          link="/constultation/categories"
-          category="Dokter THT"
-          background="https://www.saarmagazine.nl/wp-content/uploads/2017/07/dokter-.jpg"
-        />
-        <CategoryCard
-          link="/constultation/categories"
-          category="Dokter Mata"
-          background="https://www.saarmagazine.nl/wp-content/uploads/2017/07/dokter-.jpg"
-        />
+        {category &&
+          category.map((cat) => {
+            return (
+              <CategoryCard
+                key={cat.name}
+                link="/consultation/categories"
+                category={cat.name}
+                background={
+                  cat.image ||
+                  "https://www.saarmagazine.nl/wp-content/uploads/2017/07/dokter-.jpg"
+                }
+              />
+            )
+          })}
       </div>
     </>
   )
