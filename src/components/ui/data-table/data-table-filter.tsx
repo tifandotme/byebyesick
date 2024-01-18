632cac1ff8ca05864b8b3ed9d5995a93335b6581
Tifan <tifan.avianto@shopee.com>
2024-01-18 15:42:43 +0700

    refactor: edit ui components - limit table filter from select to radio (cant choose multiple value) - make `DialogContent` scrollable

--- src/components/ui/data-table/data-table-filter.tsx
+++ src/components/ui/data-table/data-table-filter.tsx
@@ -32,7 +32,7 @@ export function DataTableFilter<TData, TValue>({
   title,
   options,
 }: DataTableFilterProps<TData, TValue>) {
-  const selectedValues = new Set(column?.getFilterValue() as string[])
+  let selectedValue = column?.getFilterValue() as string | undefined
 
   return (
     <Popover>
@@ -43,38 +43,23 @@ export function DataTableFilter<TData, TValue>({
           size="sm"
           className="h-8 border-dashed"
         >
-          <PlusCircledIcon className="mr-2 h-3.5 w-3.5" />
+          <PlusCircledIcon className="mr-2 size-3.5" />
           {title}
-          {selectedValues?.size > 0 && (
+          {selectedValue && (
             <>
               <Separator orientation="vertical" className="mx-2 h-4" />
-              <Badge
-                variant="secondary"
-                className="rounded-sm px-1 font-normal lg:hidden"
-              >
-                {selectedValues.size}
-              </Badge>
               <div className="hidden space-x-1 lg:flex">
-                {selectedValues.size > 2 ? (
-                  <Badge
-                    variant="secondary"
-                    className="rounded-sm px-1 font-normal"
-                  >
-                    {selectedValues.size} selected
-                  </Badge>
-                ) : (
-                  options
-                    .filter((option) => selectedValues.has(option.value))
-                    .map((option) => (
-                      <Badge
-                        variant="secondary"
-                        key={option.value}
-                        className="rounded-sm px-1 font-normal"
-                      >
-                        {option.label}
-                      </Badge>
-                    ))
-                )}
+                {options
+                  .filter((option) => selectedValue === option.value)
+                  .map((option) => (
+                    <Badge
+                      variant="secondary"
+                      key={option.value}
+                      className="rounded-sm px-1"
+                    >
+                      {option.label}
+                    </Badge>
+                  ))}
               </div>
             </>
           )}
@@ -87,38 +72,37 @@ export function DataTableFilter<TData, TValue>({
             <CommandEmpty>No results found.</CommandEmpty>
             <CommandGroup>
               {options.map((option) => {
-                const isSelected = selectedValues.has(option.value)
+                const isSelected = selectedValue === option.value
                 return (
                   <CommandItem
                     key={option.value}
                     onSelect={() => {
                       if (isSelected) {
-                        selectedValues.delete(option.value)
+                        selectedValue = undefined
                       } else {
-                        selectedValues.add(option.value)
+                        selectedValue = option.value
                       }
-                      const filterValues = Array.from(selectedValues)
                       column?.setFilterValue(
-                        filterValues.length ? filterValues : undefined,
+                        selectedValue ? selectedValue : undefined,
                       )
                     }}
                   >
                     <div
                       className={cn(
-                        "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                        "mr-2 flex size-4 items-center justify-center rounded-full border border-primary",
                         isSelected
                           ? "bg-primary text-primary-foreground"
                           : "opacity-50 [&_svg]:invisible",
                       )}
                     >
-                      <CheckIcon className={cn("h-4 w-4")} />
+                      <CheckIcon className={cn("size-4")} />
                     </div>
                     <span>{option.label}</span>
                   </CommandItem>
                 )
               })}
             </CommandGroup>
-            {selectedValues.size > 0 && (
+            {selectedValue && (
               <>
                 <CommandSeparator />
                 <CommandGroup>
