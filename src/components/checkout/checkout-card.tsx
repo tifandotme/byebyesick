91374ae52927912bba511e6474139c0e4c95c6f0
Muhammad Yafi Al Hakim <yafi.alhakim@shopee.com>
2024-01-20 16:29:10 +0700

    feat(user): added grouping checout dta by pharmacy

--- src/components/checkout/checkout-card.tsx
+++ src/components/checkout/checkout-card.tsx
@@ -1,8 +1,10 @@
+import { format } from "path"
 import React from "react"
 import Image from "next/image"
 import { ImageIcon, Loader2 } from "lucide-react"
 
-import type { ICart, ResponseGetAll } from "@/types/api"
+import type { ICart, ICheckout, ResponseGetAll } from "@/types/api"
+import { formatPrice } from "@/lib/utils"
 import {
   Card,
   CardContent,
@@ -12,37 +14,38 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { Separator } from "@/components/ui/separator"
 
 interface CheckoutCardProps {
-  item: ICart
+  item: ICheckout
 }
 
 export function CheckoutCard({ item }: CheckoutCardProps) {
-  const [isChecked, setIsChecked] = React.useState(false)
-  const toggleCheckbox = () => setIsChecked(!isChecked)
-
   return (
     <Card className="mb-2">
-      <CardHeader className="flex flex-row items-center space-x-4 py-4">
-        <Checkbox
-          checked={isChecked}
-          onCheckedChange={toggleCheckbox}
-          className="shrink-0"
-        />
-        <CardTitle className="line-clamp-1 flex-1">
+      {/* <CardHeader className="flex flex-row items-center py-4 space-x-4">
+        <CardTitle className="flex-1 line-clamp-1">
           {item.product.name}
         </CardTitle>
-      </CardHeader>
+      </CardHeader> */}
       <Separator className="mb-4" />
       <CardContent className="pb-6 pl-6 pr-0">
         <CardDescription>
           <div className="flex items-center space-x-4">
-            <div className="relative aspect-square h-16 w-16 min-w-fit overflow-hidden rounded">
+            <div className="relative aspect-square size-16 min-w-fit overflow-hidden rounded">
               {item?.product.image?.length ? (
                 <Image
                   src={item.product.image ?? "/images/product-placeholder.webp"}
-                  alt={item.product.name ?? item.product.name}
+                  alt={item.product.name}
                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   fill
                   className="absolute object-cover"
@@ -51,27 +54,23 @@ export function CheckoutCard({ item }: CheckoutCardProps) {
               ) : (
                 <div className="flex h-full items-center justify-center bg-secondary">
                   <ImageIcon
-                    className="h-4 w-4 text-muted-foreground"
+                    className="size-4 text-muted-foreground"
                     aria-hidden="true"
                   />
                 </div>
               )}
             </div>
 
-            <div className="flex flex-col space-y-1 self-start">
-              <span className="line-clamp-1 text-sm font-medium">
+            <div className="space-y-1 self-start ">
+              <span className="line-clamp-1 text-lg text-black">
                 {item.product.name}
               </span>
 
-              <span className="line-clamp-1 text-xs text-muted-foreground">
-                {/* {formatPrice(item.product.maximum_price)} x{" "}
-                      {item.quantity} ={" "}
-                      {formatPrice(
-                        (
-                          Number(item.product.maximum_price) *
-                          Number(item.quantity)
-                        ).toFixed(2),
-                      )} */}
+              <span className="line-clamp-1 text-sm text-muted-foreground">
+                {item.quantity} x {formatPrice(item.pharmacy_product.price)}
+              </span>
+              <span className="line-clamp-1 text-sm text-muted-foreground">
+                per {item.product.selling_unit}
               </span>
             </div>
           </div>
@@ -79,17 +78,21 @@ export function CheckoutCard({ item }: CheckoutCardProps) {
       </CardContent>
       <Separator className="mb-4" />
       <CardFooter className="space-x-4">
-        <span className="flex-1">
-          {/* Total ({cartLineItems.reduce((acc, item) => acc + item.quantity, 0)}) */}
-        </span>
-        <span>
-          {/* {formatPrice(
-            cartLineItems.reduce(
-              (acc, item) => acc + Number(item.price) * item.quantity,
-              0,
-            ),
-          )} */}
-        </span>
+        <Select>
+          <SelectTrigger className="">
+            <SelectValue placeholder="Choose Shipping Method" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Fruits</SelectLabel>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </CardFooter>
     </Card>
   )
