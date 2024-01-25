30ca10a045eaeea7359a07a097d9a2b93c545750
Tifan <tifan.avianto@shopee.com>
2024-01-25 19:04:59 +0700

    fix: change api url

--- src/features/sales-report/components/section/sales-graphic/index.tsx
+++ src/features/sales-report/components/section/sales-graphic/index.tsx
@@ -77,7 +77,7 @@ function SalesGraphicSection() {
     if (sort) params.set("sort", sort.split(".")[1] as string)
     if (not_added) params.set("not_added", not_added)
 
-    return `/v1/products/admin?${params.toString()}`
+    return `/v1/products/global?${params.toString()}`
   })
 
   return (
