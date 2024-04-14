34a25099118d2781043f8a40f7e09b52c2d1cb8f
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-04-14 16:14:50 +0700

    chore: add nextjs config

---
+++ next.config.js
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+const config = {
+  output: "standalone",
+  reactStrictMode: false,
+  env: {
+    BASE_URL: process.env.NEXT_PUBLIC_DB_URL,
+  },
+  basePath: process.env.NEXT_PUBLIC_SITE_PATH,
+
+  async redirects() {
+    if (process.env.NEXT_PUBLIC_SITE_PATH === "") return []
+    return [
+      {
+        source: "/",
+        destination: `${process.env.NEXT_PUBLIC_SITE_PATH}`,
+        basePath: false,
+        permanent: false,
+      },
+    ]
+  },
+  images: {
+    unoptimized: true,
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "byebyesick-bucket.irfancen.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn0.iconfinder.com",
+      },
+    ],
+  },
+}
+
+export default config
